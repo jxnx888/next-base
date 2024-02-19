72a314acdeaac9a9465a99c91d46b8109a0d6845
Xin Ning <ningxin1007@hotmail.com>
2024-02-18 23:12:22 -0500

    feat(test): test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': 'eslint src --ext .js,.jsx,.ts,.tsx',
+  'src/**/*.{js,jsx,ts,tsx}': 'npm run lint:script',
   'src/**/*.css': 'stylelint src/**/*.css --fix',
   'src/**/*.{ts,tsx}': 'tsc --noEmit --pretty --jsx react',
 };
