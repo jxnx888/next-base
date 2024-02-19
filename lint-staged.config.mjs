01c682b5a299648ff82f7ec5c7f7ca65d5d8533e
Xin Ning <ningxin1007@hotmail.com>
2024-02-18 23:32:00 -0500

    feat(test): test

--- lint-staged.config.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  'src/**/*.{js,jsx,ts,tsx}': 'eslint src --ext .js,.jsx,.ts,.tsx',
-  'src/**/*.css': 'stylelint src/**/*.css --fix',
-  'src/**/*.{ts,tsx}': 'tsc --noEmit --pretty --jsx react',
-};
